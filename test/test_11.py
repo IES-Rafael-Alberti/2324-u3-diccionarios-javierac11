740e7bb075ebd16ecdf5da7f3affd007774bca87
javierac11 <javieracedocastrillon@gmail.com>
2023-11-09 17:26:21 +0100

    Ejercicio 11 + test

---
+++ test/test_11.py
@@ -0,0 +1,9 @@
+from src.Ejercicio11 import generaDiccionario
+
+def test_generaDiccionario():
+    texto = "nif;nombre;email;teléfono;descuento\n01234567L;Luis González;luisgonzalez@mail.com;656343576;12.5\n71476342J;Macarena Ramírez;macarena@mail.com;692839321;8\n63823376M;Juan José Martínez;juanjo@mail.com;664888233;5.2\n98376547F;Carmen Sánchez;carmen@mail.com;667677855;15.7"
+            
+    assert generaDiccionario(texto) == {'01234567L': {'nombre': 'Luis González', 'email': 'luisgonzalez@mail.com', 'teléfono': '656343576', 'descuento': 12.5},
+                                        '71476342J': {'nombre': 'Macarena Ramírez', 'email': 'macarena@mail.com', 'teléfono': '692839321', 'descuento': 8.0},
+                                        '63823376M': {'nombre': 'Juan José Martínez', 'email': 'juanjo@mail.com', 'teléfono': '664888233', 'descuento': 5.2},
+                                        '98376547F': {'nombre': 'Carmen Sánchez', 'email': 'carmen@mail.com', 'teléfono': '667677855', 'descuento': 15.7}}
